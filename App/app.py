9f48cb06ffe486a06de17232c8b1324a1e997a4c
CristhianPerdomo <cd.perdomo10@gmail.com>
2020-08-29 00:13:48 -0500

    Listo requerimiento 5

--- App/app.py
+++ App/app.py
@@ -14,6 +14,8 @@ def cmpfunction(element1,element2):
     if element1 == element2["id"]:
         return False
     return True
+
+
 def loadCSVFile (file, sep=";"):
     #lst = lt.newList("ARRAY_LIST") #Usando implementacion arraylist
     lst = lt.newList("SINGLE_LINKED") #Usando implementacion linkedlist
@@ -40,13 +42,11 @@ def printMenu():
     print("\nBienvenido")
     print("1- Cargar Datos")
     print("2- Ranking de las 10 mejores y las 10 peores peliculas")
-    print("3- Contar elementos filtrados por palabra clave")
-    print("4- Consultar elementos a partir de dos listas")
-    print("5- Consultar elementos a partir de dos listas")
-    print("6- Consultar peliculas de un director")
-    print("7- Consultar elementos a partir de dos listas")
-    print("8- Consultar elementos a partir de dos listas")
-    print("9- Consultar elementos a partir de dos listas")
+    print("3- Consultar producciones por nombre de director")
+    print("4- Consultar participaciones por actor y su principal colaboracion con un director ")
+    print("5- Consultar por genero de peliculas")
+    print("6- Consultar ranking por genero de películas")
+
     print("0- Salir")
 
 def countElementsFilteredByColumn(criteria, column, lst):
@@ -81,35 +81,42 @@ def pelismasvotadas(lst):
     ss.shellSort(lst, masvotadas)
     for contador in range(1, 11):
         pelicula = lt.getElement(lst, contador)
-        print ("{}. {}, cantidad de votos: {}". format (contador, pelicula['original_title'], pelicula["vote_count"])) 
+        print ("{}. {}, cantidad de votos: {}". format (contador, pelicula['original_title'], pelicula["vote_count"]))
+
 def pelismenosvotadas(lst):
     ss.shellSort(lst, menosvotadas)
     for contador in range(1, 11):
         pelicula = lt.getElement(lst, contador)
         print ("{}. {}, cantidad de votos: {}". format (contador, pelicula['original_title'], pelicula["vote_count"])) 
+
 def pelismejorcalificadas(lst):
     ss.shellSort(lst, mejorcalificadas)
     for contador in range(1, 11):
         pelicula = lt.getElement(lst, contador)
         print ("{}. {}, promedio de votación: {}". format (contador, pelicula['original_title'], pelicula['vote_average'])) 
+
 def pelispeorcalificadas(lst):
     ss.shellSort(lst, peorcalificadas)
     for contador in range(1, 11):
         pelicula = lt.getElement(lst, contador)
         print ("{}. {}, cantidad de votos: {}". format (contador, pelicula['original_title'], pelicula['vote_average'])) 
+
 def peliculaspordirector (listabusquedadirectores, nombrebusqueda):
     pos = lt.isPresent(listabusquedadirectores, nombrebusqueda)
     if pos:
         produccionesdirector = lt.getElement(listabusquedadirectores, pos)
         print (produccionesdirector["listapeliculas"])
+
 def masvotadas (peli1, peli2):
     if int (peli1['vote_count']) > int (peli2['vote_count']):
         return True
     return False
+
 def menosvotadas (peli1, peli2):
     if int (peli1['vote_count']) < int (peli2['vote_count']):
         return True
     return False
+
 def mejorcalificadas (peli1, peli2):
     if float (peli1['vote_average']) > float (peli2['vote_average']):
         return True
@@ -119,6 +126,8 @@ def peorcalificadas (peli1, peli2):
     if float (peli1['vote_average']) < float (peli2['vote_average']):
         return True
     return False
+
+
 #requerimiento3
 #requerimiento3
 #requerimiento3
@@ -140,9 +149,13 @@ def requerimiento3_lstdirector(lstcas,nombre,lstdet):
                  
     return None
 
+
+
 #requerimiento4
 #requerimiento4
 #requerimiento4
+
+
 
 def cmpfunction4(element1,element2):
     if element1 == element2["actor1_name"]:
@@ -175,6 +188,7 @@ def requerimiento4(lstcas,nombre,lstdet):
         directores={}
         while  it.hasNext(iterator):
             element = it.next(iterator)
+            
             if cmpfunction4(nombre,element)==False:
                 lstdet["cmpfunction"]=cmpfunction
                 r=lt.isPresent(lstdet,element["id"])
@@ -189,6 +203,53 @@ def requerimiento4(lstcas,nombre,lstdet):
             
         return lst
 
+
+#requerimiento5
+#requerimiento5
+#requerimiento5
+
+
+
+def cmpfunction_generos(element1,element2):
+    if element1 == element2["genres"]:
+        return False
+    return True
+
+def requerimiento5_generos(genero,lstdet):
+
+    if lt.isEmpty(lstdet):
+        print("La lista esta vacía")  
+        return 0
+    else:
+        iterator = it.newIterator(lstdet)
+        lst=lt.newList("SINGLE_LINKED")
+        lst["sumatoria"]=0
+        lst["genero"]=""
+        mayor=0
+        generosdict={}
+        while  it.hasNext(iterator):
+            element = it.next(iterator)
+            
+            if cmpfunction_generos(genero,element)==False:
+                lstdet["cmpfunction"]=cmpfunction
+                r=lt.isPresent(lstdet,element["id"])
+                titulo=lt.getElement(lstdet,r)
+                lt.addFirst(lst,titulo["original_title"])
+                lst["sumatoria"]+=float(titulo["vote_average"])
+                genero=element["genres"]
+                generosdict[genero]=generosdict.get(genero,0)+1
+                if generosdict[genero]>mayor:
+                    mayor=generosdict[genero]
+                    lst["genero"]=genero
+            
+        return lst
+
+
+
+
+
+
+
             
 def main():
     lista = lt.newList()   # se require usar lista definida
@@ -196,12 +257,15 @@ def main():
         printMenu() #imprimir el menu de opciones en consola
         inputs =input('Seleccione una opción para continuar\n') #leer opción ingresada
         if len(inputs)>0:
+
             if int(inputs[0])==1: #opcion 1
-                listadetalles = loadCSVFile("Data\SmallMoviesDetailsCleaned.csv") #llamar funcion cargar datos
-                listacasting = loadCSVFile("Data\MoviesCastingRaw-small.csv")
-                
+                listadetalles = loadCSVFile("Data/archivosmovies/SmallMoviesDetailsCleaned.csv") #llamar funcion cargar datos
+                listacasting = loadCSVFile("Data/archivosmovies/MoviesCastingRaw-small.csv")
                 print("Datos cargados, ",listacasting['size']," peliculas cargadas en total")
-            elif int(inputs[0])==2: #opcion 2
+
+
+
+            elif int(inputs[0])==2: #opcion 2 ranking mejores/peores peliculas - Requerimiento 2
                 if listacasting==None or listacasting['size']==0 or listadetalles==None or listadetalles['size']==0: #obtener la longitud de la lista
                     print("La lista esta vacía")    
                 else: 
@@ -223,28 +287,20 @@ def main():
                         pelispeorcalificadas(listadetalles)
                     else:
                         print ("\nLa letra escogida no corresponde a ninguna opción!")
-            elif int(inputs[0])==3: #opcion 3
-                if lista==None or lista['size']==0: #obtener la longitud de la lista
-                    print("La lista esta vacía")
-                else:   
-                    criteria =input('Ingrese el criterio de búsqueda\n')
-                    counter=countElementsFilteredByColumn(criteria, "nombre", lista) #filtrar una columna por criterio  
-                    print("Coinciden ",counter," elementos con el crtierio: ", criteria  )
-            elif int(inputs[0])==4: #opcion 4
-                if lista==None or lista['size']==0: #obtener la longitud de la lista
-                    print("La lista esta vacía")
-                else:
-                    criteria =input('Ingrese el criterio de búsqueda\n')
-                    counter=countElementsByCriteria(criteria,0,lista)
-                    print("Coinciden ",counter," elementos con el crtierio: '", criteria ,"' (en construcción ...)")
-            elif int(inputs[0])==6: #opcion 6
+
+
+
+            elif int(inputs[0])==3: #Requerimiento 3 - conocer peliculas de un director
                 if listadetalles==None or listadetalles['size']==0: #consultar_pelis_director
                     print("lista vacia")
                 else:
                     director=input("Escriba un director ")
                     directores=requerimiento3_lstdirector(listacasting,director,listadetalles)
-            elif int(inputs[0])==7: #opcion 6
-                if listadetalles==None or listadetalles['size']==0: #consultar_pelis_director
+
+
+
+            elif int(inputs[0])==4: #Requerimiento 4 - consultar por actor, sus apariciones y colaboracioes con director
+                if listadetalles==None or listadetalles['size']==0: 
                     print("lista vacia")
                 else:
                     nombre=input("Escriba un actor")        
@@ -260,6 +316,41 @@ def main():
                         while  it.hasNext(iterator):
                             element = it.next(iterator)
                             print(element)
+
+            elif int(inputs[0])==5: #Requerimiento 5 - conocer peliculas por genero, con su respectiva calificacion
+                if listadetalles==None or listadetalles['size']==0: 
+                    print("lista vacia")
+                else:
+                    genero_busqueda=input("Escriba un genero de películas que desee consultar:\n")        
+                    listageneros = requerimiento5_generos(genero_busqueda,listadetalles)
+                    
+                    
+                    if listageneros["size"]==0:
+                        print("El género ingresado no tiene coincidencias en nuestro registro")
+                    else:
+                        print("\nPara el género {}, se encontraron en total {} películas:\n" .format(genero_busqueda, str(listageneros["size"])))
+                        iterator = it.newIterator(listageneros)
+                        
+                        while  it.hasNext(iterator):
+                            element = it.next(iterator)
+                            print("->" + element)
+
+                        print ("\nEl promedio de calificación obtenido en estas películas es: "+ str(round( (listageneros["sumatoria"]/int(listageneros["size"])),2  )))
+
+                        #print (type (listageneros))
+                        #print (listageneros.keys())
+                        #print (listageneros['first'])
+                        #print (element)
+
+            elif int(inputs[0])==6: #Requerimiento 6 - rankings por genero 
+                if listadetalles==None or listadetalles['size']==0: 
+                    print("lista vacia")
+                else:
+                    director=input("Escriba un director ")
+                    directores=requerimiento3_lstdirector(listacasting,director,listadetalles)
+
+
+
             elif int(inputs[0])==0: #opcion 0, salir
                 sys.exit(0)
                 
